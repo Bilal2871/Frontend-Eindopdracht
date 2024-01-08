48ef38a2373257b2a5747e0d8d30063a85f2a375
Bilal2871 <bilalabchrim@gmail.com>
2024-01-08 15:08:05 +0100

    Start for the navigation.

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -7,32 +7,47 @@ import './navigation.css';
 
 function Navigation(){
     return (
-        <section>
-            <div id="navBarContainer"> 
-                <nav>
-                    <ul>
-                        {/* <li>
-                            <img src={pokeball} alt="" srcset="" id="logoPokeball"/>
-                        </li> */}
-                        <li>
-                            <img src={pokemon} alt="" id="logoPokemon"/>
-                        </li>
-                        {/* <li>
-                        <NavLink to="/overview">Home</NavLink>
-                        </li>
-                        <li>
-                        <NavLink to="/contact">Contact</NavLink>
-                        </li>
-                        <li>
-                            <p>favoriet</p>
-                        </li>
-                        <li>
-                            inloggen
-                        </li> */}
-                    </ul>
-                </nav>
-            </div>
-        </section>
+        // <section>
+        //     <div id="navBarContainer"> 
+        //         <nav>
+        //             <ul>
+        //                 {/* <li>
+        //                 </li> */}
+        //                 <li>
+        //                     <img src={pokemon} alt="" id="logoPokemon"/>
+        //                 </li>
+        //                 {/* <li>
+        //                 </li>
+        //              
+        //                 <li>
+        //                     <p>favoriet</p>
+        //                 </li>
+        //                 <li>
+        //                     inloggen
+        //                 </li> */}
+        //             </ul>
+        //         </nav>
+        //     </div>
+        // </section>
+
+        <section class="top-nav">
+        <div>
+            <img src={pokeball} alt="" srcset="" id="logoPokeball"/>
+        </div>
+        <div>
+          <img src={pokemon} alt="" id="logoPokemon"/>
+        </div>
+        <input id="menu-toggle" type="checkbox" />
+        <label class='menu-button-container' for="menu-toggle">
+        <div class='menu-button'></div>
+      </label>
+        <ul class="menu">
+          <li>Inloggen/registreren</li>
+          <li><NavLink to="/overview">Overzicht</NavLink></li>
+          <li><NavLink to="/contact">Contact</NavLink></li>
+          <li>Favorieten</li>
+        </ul>
+      </section>
     )
 }
 
