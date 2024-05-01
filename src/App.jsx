b9149a17b2ce57a9039075ea061ff15bfaa31263
Bilal2871 <bilalabchrim@gmail.com>
2024-05-01 13:32:21 +0200

    Added folders and a few other things.

--- src/App.jsx
+++ src/App.jsx
@@ -5,33 +5,30 @@ import Overview from './pages/overview/overview';
 import Contact from './pages/contact/contact';
 import Navigation from './components/navigation/navigation';
 import FooterBar from './components/footerBar/footer';
-import Pokemons from './components/allPokemons/allPokemons';
 
 function App() {
 
   return (
     <>
       <Navigation/>
-    
-      <Routes>
-      <Route path = "/"             element = {<Overview/>} />
-          <Route path = "/overview"     element = {<Overview/>} />
-          <Route path = "/pokemonDetail" element = {<PokemonDetail/>} />
-          <Route path = "/contact"      element = {<Contact/>}  />
-          <Route path = "/register"      element = {<Register/>}  />
-          <Route path = "/login"      element = {<Login/>}  />
-          <Route path = "/favorites"    element = {<Favorites/>} />
+        <Routes>
+        <Route path = "/"             element = {<Overview/>} />
+            <Route path = "/overview"     element = {<Overview/>} />
+            <Route path = "/pokemonDetail" element = {<PokemonDetail/>} />
+            <Route path = "/contact"      element = {<Contact/>}  />
+            <Route path = "/register"      element = {<Register/>}  />
+            <Route path = "/login"      element = {<Login/>}  />
+            <Route path = "/favorites"    element = {<Favorites/>} />
             <Route path = "/account"      element = {<AccountOverview/>} />
-          <Route path = "*" element = {<Overview/>} />
-          
+            <Route path = "*" element = {<Overview/>} />
+            
 
-        {/* <Route path="*" element={<NotFound />} /> deze nog erin zetten
-                <Route path = "/"             element = {<Overview/>} />
-        <Route path = "/overview"     element = {<Overview/>} />
-        <Route path = "/allPokemons"  element = {<Pokemons/>} />
-        <Route path = "/contact"      element = {<Contact/>}  /> */}
-      </Routes> 
-      
+          {/* <Route path="*" element={<NotFound />} /> deze nog erin zetten
+                  <Route path = "/"             element = {<Overview/>} />
+          <Route path = "/overview"     element = {<Overview/>} />
+          <Route path = "/allPokemons"  element = {<Pokemons/>} />
+          <Route path = "/contact"      element = {<Contact/>}  /> */}
+        </Routes> 
       <FooterBar/>
     </>
   )
