16feb780f0a27782a9359a9533de287c79de9172
Bilal2871 <bilalabchrim@gmail.com>
2024-05-02 14:32:34 +0200

    The pagination.

---
+++ src/components/allPokemons/pagination.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Pagination({ currentPage, totalPages, beginPage, prevPage, nextPage, endPage }) {
+    return (
+        <div className="buttons">
+            <button onClick={beginPage} disabled={currentPage === 1}>Begin</button>
+            <button onClick={prevPage} disabled={currentPage === 1}>Vorige</button>
+            <button onClick={nextPage} disabled={currentPage === totalPages}>Volgende</button>
+            <button onClick={endPage} disabled={currentPage === totalPages}>Einde</button>
+        </div>
+    );
+}
+
+export default Pagination;
