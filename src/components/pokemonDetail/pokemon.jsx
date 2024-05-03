dbd74275b94d8c3e08ab43d5a678c47cd56f1e61
Bilal2871 <bilalabchrim@gmail.com>
2024-05-03 12:29:38 +0200

    Added the pokemon detail page and css.

--- src/components/pokemonDetail/pokemon.jsx
+++ src/components/pokemonDetail/pokemon.jsx
@@ -1,15 +1,38 @@
+import './pokemonDetail.css';
+import React, { useState, useEffect } from 'react';
 
+export default function PokemonDetail({ pokemon, onClose }) {
 
-const Pokemon = ({id, name, image,type}) => {
     return (
-        <div className={style}>
-            <div className="#"> #0{id}</div>
-            <img src={image} alt={name} />
-            <div className="#">
-            
-            <h3>{name}</h3>
-            <small>Type: {type}</small>
-        </div>
-    </div>
-    )
+        <>
+            <div className="overlay"></div>
+            <div className="pokemon-detail-popup">
+                <button onClick={onClose}>X</button>
+                <div className="pokemon-image">
+                    <img src={pokemon.sprites.front_default} alt={pokemon.name}/>
+                </div>
+                <div className="pokemon-data">
+                    <h2>{pokemon.name}</h2>
+                    <p>Lengte: {pokemon.height}</p>
+                    <p>Gewicht: {pokemon.weight}</p>
+                    <p>Type: {pokemon.types.map(type => type.type.name).join(', ')}</p>
+                    <p>Krachten: {pokemon.abilities.map(ability => ability.ability.name).join(', ')}</p>
+                </div>
+            </div>
+        </>
+    );
 }
+
+// const Pokemon = ({id, name, image,type}) => {
+//     return (
+//         <div className={style}>
+//             <div className="#"> #0{id}</div>
+//             <img src={image} alt={name} />
+//             <div className="#">
+            
+//             <h3>{name}</h3>
+//             <small>Type: {type}</small>
+//         </div>
+//     </div>
+//     )
+// }
