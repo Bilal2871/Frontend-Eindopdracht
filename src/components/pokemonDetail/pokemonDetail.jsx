f3d84178a305ccd87f33a07417ea5aeb8150830b
Bilal2871 <bilalabchrim@gmail.com>
2024-05-05 23:35:42 +0200

    Deleted unneseccary code.

--- src/components/pokemonDetail/pokemonDetail.jsx
+++ src/components/pokemonDetail/pokemonDetail.jsx
@@ -22,17 +22,3 @@ export default function PokemonDetail({ pokemon, onClose }) {
         </>
     );
 }
-
-// const Pokemon = ({id, name, image,type}) => {
-//     return (
-//         <div className={style}>
-//             <div className="#"> #0{id}</div>
-//             <img src={image} alt={name} />
-//             <div className="#">
-            
-//             <h3>{name}</h3>
-//             <small>Type: {type}</small>
-//         </div>
-//     </div>
-//     )
-// }
